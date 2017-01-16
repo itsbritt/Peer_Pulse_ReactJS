7df92dc8d41e555a8398e3cf660f4f91f4e1ef4d
brittshroyer <britt.shroyer@gmail.com>
2017-01-16 16:46:44 -0600

    login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
-import { Button, Col } from 'react-bootstrap';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 
@@ -39,17 +37,10 @@ class App extends Component {
 
   render() {
     const welcomeMessage = (firebase.auth().currentUser) ?
-      <h4>Welcome { this.state.user.displayName }!</h4> :
+      <h4>Hi { this.state.user.displayName }!</h4> :
       '';
 
     return (
-
-      <Col xs={12} md={12}>
-      <div className="App">
-        <div className="App-header">
-
-          <h2>Welcome to React</h2>
-
       <div className="container">
         { this.sessionButton() }
         <h1>Peer Pulse</h1>
@@ -58,10 +49,8 @@ class App extends Component {
 
         <div className="content">
           { this.props.children}
-
         </div>
       </div>
-    </Col>
     );
   }
 }
