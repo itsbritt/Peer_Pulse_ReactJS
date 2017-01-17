8bcf5645e9c011df5a76bed8f93302d3e95b3e62
Stephanie Workman <sworkman2014@gmail.com>
2017-01-16 18:36:47 -0600

    moved contents of App.js to Home.js and added button

--- src/App.js
+++ src/App.js
@@ -3,48 +3,25 @@ import './App.css';
 
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
+import Home from './components/Home';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router';
 
 import {firebase} from './utils/firebase';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      user: {}
-    }
-  }
-//lifecycle method: if user is logged in, show user object, if not show empty object
-  componentWillMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        console.log('Logged in:', user);
-        this.setState({ user });
-      } else {
-        this.setState({ user: {} });
-      }
-    });
-  }
-//if someone is not logged in, show login..if not show logout button and name of person,
-  sessionButton() {
-    if (!firebase.auth().currentUser) {
-      return <LoginButton>Log in with GitHub</LoginButton>;
-    } else {
-      return <LogoutButton>Logout { this.state.user.displayName }</LogoutButton>;
-    }
-  }
 
   render() {
-    const welcomeMessage = (firebase.auth().currentUser) ?
-      <h4>Hi { this.state.user.displayName }!</h4> :
-      '';
 
     return (
       <div className="background">
-        { this.sessionButton() }
         <h1 className="header">Peer-Pulse</h1>
-        { welcomeMessage }
+        <div>
+          <Button>
+            <Link to="/home"><h2>get started</h2></Link>
+          </Button>
+        </div>
       </div>
     );
   }
