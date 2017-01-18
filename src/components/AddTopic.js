bcfe48b431d4af7cabace6b8ac01ccafda374348
vinh huynh <vvvhuynh@yahoo.com>
2017-01-17 19:15:37 -0600

    added Dirks firebase array functionality into AddTopic.js

--- src/components/AddTopic.js
+++ src/components/AddTopic.js
@@ -17,11 +17,29 @@ class AddTopic extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-
+    // let newArr;
+    const topicsRef = firebase.database().ref("topics/");
     const topicTitle = this.refs.topicTitle.value;
     const description = this.refs.description.value;
-    const idea = this.refs.idea.value;
+    const idea = [this.refs.idea.value];
     const userid = firebase.auth().currentUser.uid
+
+    const ideaNew = this.refs.ideaAdd.value;
+
+    // if (newArr) {
+    //   newArr = newArr.concat(ideaNew)
+    // } else {
+    //   newArr = idea.concat(ideaNew)
+    // }
+
+    let newArr = idea.concat(ideaNew)
+
+    topicsRef.orderByChild("title").equalTo(topicTitle).once("value",(data)=>{
+      for (let theKey in data.val()) {
+        console.log('key is: ', theKey);
+        topicsRef.child(theKey).update({ "idea": newArr })
+      }
+    });
 
     firebase.database()
       .ref('/topics')
@@ -63,6 +81,13 @@ class AddTopic extends Component {
             <Button>
               <h3>Add another idea</h3>
             </Button>
+
+            <input
+              className="form-control"
+              type="text"
+              ref="ideaAdd"
+              placeholder="Add new Idea" />
+
             <form onSubmit={ this.handleSubmit.bind(this) }>
             <input
               className="btn btn-primary"
